be7bfade66fd41c9facdbe7a345a566ea5657bc1
Nick Howland <nhowland@outlook.com>
2020-01-16 16:39:57 -0500

    daily update

--- src/App.js
+++ src/App.js
@@ -2,8 +2,15 @@ import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import axios from 'axios';
 
 class App extends Component { // extends the core react component, used to create a class which is a child of another class. 
+  async componentDidMount() {
+    // can use Axios to make request to github api
+    const res = await axios.get('https://api.github.com/users')
+    console.log(res.data);
+  }
+
   render() { // a lifecycle method that runs at a certain point when component is loaded
     return (
       <div className="App">
