da69343df0c09b5a06bbce2c49696478caf96d49
Nick Howland <nhowland@outlook.com>
2020-01-23 16:42:15 -0500

    add non-hard coded user list

--- src/App.js
+++ src/App.js
@@ -5,10 +5,17 @@ import Users from './components/users/Users';
 import axios from 'axios';
 
 class App extends Component { // extends the core react component, used to create a class which is a child of another class. 
+  state = {
+    users: [],
+    loading: false
+  }
   async componentDidMount() {
     // can use Axios to make request to github api
+    this.setState({ loading: true });
+
     const res = await axios.get('https://api.github.com/users')
-    console.log(res.data);
+    // after getting response, we want to reset the state. 
+    this.setState({ users: res.data, loading: false });
   }
 
   render() { // a lifecycle method that runs at a certain point when component is loaded
@@ -16,7 +23,7 @@ class App extends Component { // extends the core react component, used to creat
       <div className="App">
           <Navbar />
           <div className="container">
-          <Users />
+            <Users loading={this.state.loading} users={this.state.users} />
           </div>
       </div>
     );
