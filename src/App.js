9c58c37a120886a81b8dd1d69ae31fd8d3a0e16f
Nick Howland <nhowland@outlook.com>
2020-01-24 18:11:36 -0500

    added nav bar features and state component

--- src/App.js
+++ src/App.js
@@ -1,31 +1,70 @@
-import React, { Component } from 'react';
+import React, { Fragment, Component } from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import Search from './components/users/Search';
 import axios from 'axios';
+import Alert from './components/layout/Alert';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'; // need to wrap everything in the render dom
+import About from './components/pages/About';
 
 class App extends Component { // extends the core react component, used to create a class which is a child of another class. 
   state = {
     users: [],
-    loading: false
+    loading: false,
+    alert: null
   }
-  async componentDidMount() {
-    // can use Axios to make request to github api
+
+// search github users
+  searchUsers = async text => {
     this.setState({ loading: true });
 
-    const res = await axios.get('https://api.github.com/users')
-    // after getting response, we want to reset the state. 
-    this.setState({ users: res.data, loading: false });
-  }
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&?client_id=${
+        process.env.REACT_APP_GITHUB_CLIENT_ID
+      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    // after getting response, we want to reset the state to the users that are returned on the search. 
+    this.setState({ users: res.data.items, loading: false });
+  };
+
+  // Clear users from state
+  clearUsers = () => this.setState({ users: [], loading: false });
+
+  // Set Alert
+
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } });
+    setTimeout(() => this.setState({ alert: null}), 5000);
+  };
 
   render() { // a lifecycle method that runs at a certain point when component is loaded
+    const { users, loading } = this.state;
+
     return (
+      <Router>
       <div className="App">
           <Navbar />
           <div className="container">
-            <Users loading={this.state.loading} users={this.state.users} />
+            <Alert alert={this.state.alert} />
+            <Switch>
+              <Route exact path='/' render={props => (
+                <Fragment>
+                  <Search 
+                    searchUsers={this.searchUsers} 
+                    clearUsers={this.clearUsers} 
+                    showClear={users.length > 0 ? true : false}
+                    setAlert={this.setAlert}
+                  />
+                  <Users loading={loading} users={users} />
+                </Fragment>
+              )}
+              />
+              <Route exact path ='/about' component={About} />
+            </Switch>
           </div>
       </div>
+      </Router>
     );
   }
 }
