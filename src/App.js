86844bf9bef4454731a146e6a60f3923729397e5
Nick Howland <nhowland@outlook.com>
2020-01-27 16:19:04 -0500

    finished but need to refactor to hooks

--- src/App.js
+++ src/App.js
@@ -7,10 +7,12 @@ import axios from 'axios';
 import Alert from './components/layout/Alert';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'; // need to wrap everything in the render dom
 import About from './components/pages/About';
+import User from './components/users/User'; // need to create a route different from Users, because it munst pass in props
 
 class App extends Component { // extends the core react component, used to create a class which is a child of another class. 
   state = {
     users: [],
+    user: {},
     loading: false,
     alert: null
   }
@@ -28,6 +30,19 @@ class App extends Component { // extends the core react component, used to creat
     this.setState({ users: res.data.items, loading: false });
   };
 
+  // Get a single github user and display information
+  getUser = async username => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(
+      `https://api.github.com/users/${username}?client_id=${
+        process.env.REACT_APP_GITHUB_CLIENT_ID
+      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ user: res.data, loading: false });
+  }
+
   // Clear users from state
   clearUsers = () => this.setState({ users: [], loading: false });
 
@@ -39,11 +54,11 @@ class App extends Component { // extends the core react component, used to creat
   };
 
   render() { // a lifecycle method that runs at a certain point when component is loaded
-    const { users, loading } = this.state;
+    const { users, user, loading } = this.state;
 
     return (
       <Router>
-      <div className="App">
+        <div className="App">
           <Navbar />
           <div className="container">
             <Alert alert={this.state.alert} />
@@ -61,9 +76,16 @@ class App extends Component { // extends the core react component, used to creat
               )}
               />
               <Route exact path ='/about' component={About} />
+              <Route exact path='/user/:login' render={props => (
+                <User {...props } 
+                getUser={this.getUser} 
+                user={user} 
+                loading={loading} 
+                />
+              )} />
             </Switch>
           </div>
-      </div>
+        </div>
       </Router>
     );
   }
